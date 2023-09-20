c644240687250caf2cfa816a5948deaf966b4b82
Maira Pajaro <maira_pajaro@hotmail.com>
2023-09-20 12:28:42 +0200

    basic challenge ended 6

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+//example with a method:
+
+// function findMaxNumber(arr) {
+//   return Math.max(...arr);
+// }
 
 module.exports = findMaxNumber;
--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(" "); //split the words to separate
+
+  for (let i = 0; i < words.length; i++) {
+    //loop through
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1); //words at index 0 transform to uppercase and add the other letters cutting out the first one.
+  }
+  return words.join(" "); // return words with a join separate by space so it gets all the phrase in one string.
+}
 
 module.exports = titleCase;
